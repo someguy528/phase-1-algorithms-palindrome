9da7585319f027605edd6eb4b374fec7ab5f817c
Brandon Castro <castroyou999@gmail.com>
2022-09-29 22:40:40 -0400

    commit tree 93296fa9895a22a35e3e82e064f294aea9d07142

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let firstLetter = 0;
+  let lastLetter = word.length - 1;
+  while (firstLetter < lastLetter){
+    if(word[firstLetter] !== word[lastLetter]){
+      return false
+    }
+    else{
+      firstLetter += 1;
+      lastLetter -=1;
+    }
+  }
+  return true
 }
 
 /* 
   Add your pseudocode here
 */
+// check to see if first & last index are equal, then add 1 to first index and subtract 1 from last index 
+//   while first index < last index, repeat to see if equal
 
 /*
   Add written explanation of your solution here
 */
+  // returns false if word fails test, if passes the while loop it should return true
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
